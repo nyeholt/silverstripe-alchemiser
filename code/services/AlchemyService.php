0d0783cc16431848e01108d3cc70466135ea1f88
ajshort <andrewjshort@gmail.com>
2011-04-09 23:42:20 +1000

    Refactored AlchemyService to use RestfulService rather than WebApiClient.

--- code/services/AlchemyService.php
+++ code/services/AlchemyService.php
@@ -1,33 +1,10 @@
 <?php
-/*
-
-Copyright (c) 2009, SilverStripe Australia PTY LTD - www.silverstripe.com.au
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-
-    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of SilverStripe nor the names of its contributors may be used to endorse or promote products derived from this software
-      without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
-OF SUCH DAMAGE.
-*/
-
 /**
  * A service that uses the AlchemyAPI to retrieve information about content
  *
  * @author Marcus Nyeholt <marcus@silverstripe.com.au>
  */
-class AlchemyService
-{
-    protected $api;
+class AlchemyService {
 
 	/**
 	 * How many characters should content have before it is indexed?
@@ -35,7 +12,7 @@ class AlchemyService
 	 * @var int
 	 */
 	public static $char_limit = 80;
-	
+
 	/**
 	 * How many keywords should we use?
 	 *
@@ -44,139 +21,86 @@ class AlchemyService
 	public static $max_keywords = 15;
 
 	public static $config = array(
-		'api_url' => 'http://access.alchemyapi.com',
-		'api_key' => '',
+		'api_url'  => 'http://access.alchemyapi.com',
+		'api_path' => '/calls/text/',
+		'api_key'  => '',
 	);
 
 	public static function set_api_key($key) {
 		self::$config['api_key'] = $key;
 	}
 
-	public function  __construct() {
-		$this->api = new WebApiClient(self::$config['api_url']);
-		$this->api->setMethods(self::$methods);
-		$this->api->setGlobalParam('apikey', self::$config['api_key']);
-		$this->api->setGlobalParam('outputMode', 'json');
+	/**
+	 * @var RestfulService
+	 */
+	protected $api;
+
+	public function __construct() {
+		$this->api = new RestfulService(Controller::join_links(
+			self::$config['api_url'], self::$config['api_path']
+		));
 	}
 
 	/**
-	 * Updates a content item with data found by alchemy.
+	 * Updates a content item with data from the Alchemy API.
 	 *
-	 * Note that this does NOT save the object once finished. Callers must handle that themselves
-	 *
-	 * @param SiteTree $object
+	 * @param DataObject $object
 	 */
-	public function alchemise($object, $force=false) {
-		
-		$fields = $object->stat('extraction_fields');
-		if (!$fields) {
+	public function alchemise(DataObject $object) {
+		$content = '';
+
+		if (!$fields = $object->stat('extraction_fields')) {
 			$fields = array('Title', 'Content');
 		}
-		$content = '';
-		foreach ($fields as $name) {
-			if ($object->hasField($name) && ($force || $object->isChanged($name))) {
-				$content .= ' ' . $object->$name;
-			}
+
+		foreach ($fields as $field) if ($object->hasField($field)) {
+			$content .= strip_tags($object->obj($field)->forTemplate()) . ' ';
 		}
 
-		// clean it up a bit - might need to be a bit more forceful with this. 
-		$content = strip_tags($content);
-
-		// Need to have a reasonable amount of content change before indexing...
 		if (strlen($content) < self::$char_limit) {
 			return;
 		}
 
-		$result = null;
-		try {
-			$result = $this->getEntities($content);
-		} catch (Exception $e) {
-			SS_Log::log($e, SS_Log::ERR);
-		}
+		$this->api->setQueryString(array(
+			'apikey' => self::$config['api_key'],
+			'text'   => $content
+		));
 
-		if ($result && $result->status == 'OK' && isset($result->entities) && count($result->entities)) {
-			foreach ($result->entities as $entity) {
-				$field = 'Alc'.$entity->type;
-				if (!$object->hasField($field)) {
-					singleton('AlcUtils')->log("Alchemy returned field $field but it was not available on object $object->ID", SS_Log::WARN);
-					continue;
-				}
-				// make sure the field is empty because we're adding new data in
-				$object->$field = array();
-			}
+		$entities = $this->api->request('TextGetRankedNamedEntities');
+		$seen     = array();
 
-			foreach ($result->entities as $entity) {
-				$field = 'Alc'.$entity->type;
+		if (!$entities->isError()) {
+			foreach ($entities->simpleXML()->entities->entity as $entity) {
+				$name      = "Alc{$entity->type}";
 				$relevance = $entity->relevance;
-				if ($relevance > 0.3) {
-					if (!$object->hasField($field)) {
-						singleton('AlcUtils')->log("Alchemy returned field $field but it was not available on object $object->ID", SS_Log::WARN);
-						continue;
-					}
-					$cur = $object->$field->getValues();
-					$cur[] = $entity->text;
-					$object->$field = $cur;
+
+				if (!array_key_exists($name, $seen)) {
+					$object->$name = array();
+					$seen[$name] = true;
+				}
+
+				if ($relevance > .3) {
+					$values = $object->$name->getValues();
+					$values[] = (string) $entity->text;
+					$object->$name = $values;
 				}
 			}
 		}
 
-		try {
-			usleep(500);
-			$result = $this->getKeywords($content);
-		} catch (Exception $e) {
-			SS_Log::log($e, SS_Log::ERR);
-		}
+		$keywords = $this->api->request('TextGetRankedKeywords');
+		$total    = 0;
 
-		if ($result && $result->status == 'OK' && isset($result->keywords) && count($result->keywords)) {
-			$keywords = array();
-			 
-			for ($i = 0, $c = count($result->keywords); $i < $c && $i < self::$max_keywords; $i++) {
-				$keyword = $result->keywords[$i];
-				$keywords[] = $keyword->text;
+		if (!$keywords->isError()) {
+			$extracted = array();
+
+			foreach ($keywords->simpleXML()->keywords->keyword as $keyword) {
+				if ($total++ < self::$max_keywords) {
+					$extracted[] = (string) $keyword->text;
+				}
 			}
-			$object->AlcKeywords = $keywords;
-		} else {
-			singleton('AlcUtils')->log("There was an error getting keywords for $object->ID, result is ".var_export($result, true), SS_Log::ERR);
+
+			$object->AlcKeywords = $extracted;
 		}
 	}
 
-	public function getEntities($content) {
-		return $this->call('getEntities', array('text' => $content));
-	}
-
-	public function getKeywords($content) {
-		return $this->call('getKeywords', array('text' => $content));
-	}
-
-	public function call($method, $args = null) {
-		return $this->api->callMethod($method, $args);
-	}
-
-
-	public static $methods = array(
-		'getEntities' => array(
-			'method' => 'POST',
-			'url' => '/calls/text/TextGetRankedNamedEntities',
-			'params' => array('text'),
-			'cache' => 600,
-			'return' => 'json'
-			// 'enctype' => Zend_Http_Client::ENC_FORMDATA,
-		),
-		'getCategory' => array(
-			'method' => 'POST',
-			'url' => '/calls/text/TextGetCategory',
-			'params' => array('text'),
-			'cache' => 600,
-			'return' => 'json'
-		),
-		
-		'getKeywords' => array(
-			'method' => 'POST',
-			'url' => '/calls/text/TextGetRankedKeywords',
-			'params' => array('text'),
-			'cache' => 600,
-			'return' => 'json'
-		),
-	);
 }
-?>
